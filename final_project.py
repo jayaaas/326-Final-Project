4f8cc64bb7b7a95045dcbd109441d56d9f935dc0
jayaaas <115901301+jayaaas@users.noreply.github.com>
2023-10-30 18:32:49 -0400

    check-in 1

---
+++ final_project.py
@@ -0,0 +1,25 @@
+#Movie class
+#contains file that has all movies in database (need to find movie database)
+
+#__init__
+'''initializes movie class'''
+
+#clean_up function
+''' cleans up data for each movie, makes it easier to access and parse through data
+args: file that has data for each movie 
+returns: cleaned up version of information about movie 
+'''
+
+#check_movie function
+'''check for movie within dataset to ensure it fits criteria that user asks for
+args: genre, rating, duration
+returns: name of movie'''
+
+#get_matches function
+'''finds a list of matches to display to user
+returns: list of movies and where user can watch it'''
+
+#filter_movies
+'''helps user filter out results
+args: result of get_matches
+returns: more specific list of movies'''
