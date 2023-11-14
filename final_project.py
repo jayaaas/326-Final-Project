d907c377489f7a7c7e76b737ca4313baaec011a5
alc24 <a.craun1@gmail.com>
2023-11-14 17:30:36 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -12,13 +12,23 @@ class Movie():
     def clean_up(movies):
         ''' cleans up data for each movie, makes it easier to access and parse through data
 args: file that has data for each movie 
-returns: cleaned up version of information about movie 
+returns: tuple of information we want about movie 
 '''
     pass
 
 assert(movie_data) == "Soul Surfer, Drama, PG, 1.75 hours"
 assert(movie_data) == "Barbie, Fantasy, PG-13, 2 hours"
 assert(movie_data) == "Old Dads, Comedy, R, 1.75 hours"
+
+def user_pref():
+    ''' Gets information from the user about what they are looking for in a movie
+    args: 
+    '''
+    
+def choose_movie():
+    ''' Asks the user what movie they want to search for information about
+    from the file
+    '''
 
 #check_movie function
 def check_movie(genre, rating, duration):
@@ -32,6 +42,10 @@ assert("fantasy", "PG", 1.5) == "Shrek"
 assert("family", "G", 2) == "Cars"
 assert("horror", "PG-13", 1.5) == "The Boogeyman"
 
+def display_info():
+    ''' displays information about specific movie they want to search for
+    '''
+
 #get_matches function
 def get_matches():
     '''finds a list of matches to display to user
@@ -42,11 +56,15 @@ assert(user_inputs) == ["Murder Mystery, Grown Ups, The Do-Over, Blended, Just G
 assert(user_inputs) == ["Shrek, Cars, Mall Cop, The Lorax, Planes, Wall-e"]
 
 #filter_movies
-def filter_movies(results):
-    '''helps user filter out results
+def display_results(results):
+    ''' Displays the matches to user criteria in a readable way
 args: result of get_matches
-returns: more specific list of movies'''
+returns: text of the movies that match criteria'''
     pass
 
 assert(get_matches) == ["Grown-Ups, Blended"]
 assert(get_matches) == ["Cars, Planes"]
+
+def main():
+    ''' main function 
+    '''
