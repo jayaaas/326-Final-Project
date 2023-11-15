42859a23b3027748067bd4e7ae2713766ec071f8
alc24 <a.craun1@gmail.com>
2023-11-15 18:10:42 -0500

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -1,18 +1,19 @@
 #Movie class
 #contains file that has all movies in database (need to find movie database)
 class Movie():
-    
 
 #__init__
-    '''initializes movie class'''
+    '''Initialize all of the attributes of the class'''
     def __init__(self):
         pass
 
 #clean_up function
-    def clean_up(movies):
-        ''' cleans up data for each movie, makes it easier to access and parse through data
-args: file that has data for each movie 
-returns: tuple of information we want about movie 
+    def get_data(path):
+        ''' Gets values from the movies.csv file and stores them into variables.
+        Only gets the data that we want from the file (title, genre, rating,
+        description, and director) and disregards the other pieces of information. 
+        
+        Returns: a list of movies with movie objects in it.
 '''
     pass
 
@@ -20,46 +21,46 @@ assert(movie_data) == "Soul Surfer, Drama, PG, 1.75 hours"
 assert(movie_data) == "Barbie, Fantasy, PG-13, 2 hours"
 assert(movie_data) == "Old Dads, Comedy, R, 1.75 hours"
 
-def user_pref():
-    ''' Gets information from the user about what they are looking for in a movie
-    args: 
+def choose_movie():
+    ''' Ask the user what movie they want to search for and finds it within
+    the list of movies created
+    
+    Returns: a movie object that matches what the user searched for 
     '''
     
-def choose_movie():
-    ''' Asks the user what movie they want to search for information about
-    from the file
+def display_info():
+    ''' Displays information about movie in a readable way.
+    
     '''
-
-#check_movie function
-def check_movie(genre, rating, duration):
-    '''check for movie within dataset to ensure it fits criteria that user asks for
-args: genre, rating, duration
-returns: name of movie'''
-    pass
 
 assert("comedy", "R", 1.75) == "Hot Tub Time Machine"
 assert("fantasy", "PG", 1.5) == "Shrek"
 assert("family", "G", 2) == "Cars"
 assert("horror", "PG-13", 1.5) == "The Boogeyman"
 
-def display_info():
-    ''' displays information about specific movie they want to search for
+def user_pref():
+    ''' Ask the user a number of questions to find out what preferences they
+    have for the movie they want to watch.
+    
+    Returns: a tuple of all of the preferences the user has
     '''
 
 #get_matches function
 def get_matches():
-    '''finds a list of matches to display to user
-returns: list of movies that matches what the user put in'''
+    '''Finds a list of matches to user preference from the movie list
+
+    Returns: list of movies that match what the user prefers'''
     pass
 
 assert(user_inputs) == ["Murder Mystery, Grown Ups, The Do-Over, Blended, Just Go With It"]
 assert(user_inputs) == ["Shrek, Cars, Mall Cop, The Lorax, Planes, Wall-e"]
 
 #filter_movies
-def display_results(results):
+def display_results():
     ''' Displays the matches to user criteria in a readable way
-args: result of get_matches
-returns: text of the movies that match criteria'''
+    
+    args: result of get_matches
+    returns: text of the movies that match criteria'''
     pass
 
 assert(get_matches) == ["Grown-Ups, Blended"]
