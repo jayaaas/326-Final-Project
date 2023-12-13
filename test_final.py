086e4a2f482cd2a952f616bc3cb59ea0fbb41496
juliacalvert <147757218+juliacalvert@users.noreply.github.com>
2023-12-13 15:25:07 -0500

    update

--- test_final.py
+++ test_final.py
@@ -26,60 +26,10 @@ def database_put_info_test():
     assert 5.3 in database.scores
 
 
-#finish these below, I think they're all set up the same?
-def database_get_data_test():
-    database = Database()
-    movie = Movie("Popeye", "Adventure", "PG", "Robert Altman", 1980, 5.3)
-    database.movies.append(movie)
-    database.get_data()
-    pass
-
-
-def database_choose_movie_test():
-    database = Database()
-    movie = Movie("Popeye", "Adventure", "PG", "Robert Altman", 1980, 5.3)
-    database.movies.append(movie)
-    database.choose_movie()
-    pass
-
-def database_display_info_test():
-    database = Database()
-    movie = Movie("Popeye", "Adventure", "PG", "Robert Altman", 1980, 5.3)
-    database.movies.append(movie)
-    database.display_info()
-    pass
-
-def database_user_pref_test():
-    database = Database()
-    movie = Movie("Popeye", "Adventure", "PG", "Robert Altman", 1980, 5.3)
-    database.movies.append(movie)
-    database.user_pref()
-    pass
-
-def database_get_matches_test():
-    database = Database()
-    movie = Movie("Popeye", "Adventure", "PG", "Robert Altman", 1980, 5.3)
-    database.movies.append(movie)
-    database.get_matches()
-    pass
-
-
-def database_display_results_test():
-    database = Database()
-    movie = Movie("Popeye", "Adventure", "PG", "Robert Altman", 1980, 5.3)
-    database.movies.append(movie)
-    database.display_results()
-    pass
-
 
 
 test_movie_class()
 database_put_info_test()
-database_get_data_test()
-database_choose_movie_test()
-database_display_info_test()
-database_user_pref_test()
-database_get_matches_test()
-database_display_results_test()
+
 
 print("No errors. Functions get what they should.")
