feeb46ecba3f5cb62db869c4245f182dbb8af34f
jayaaas <115901301+jayaaas@users.noreply.github.com>
2023-12-15 03:39:16 -0500

    Update test_final.py

--- test_final.py
+++ test_final.py
@@ -9,6 +9,7 @@ movie = Movie("Popeye", "Adventure", "PG", "Robert Altman", 1980, 5.3)
 database = Database()
 database.movies.append(movie)
 
+#unit test movie class
 def test_movie_class():
     assert movie.title == "Popeye"
     assert movie.genre == "Adventure"
@@ -17,6 +18,7 @@ def test_movie_class():
     assert movie.year == 1980
     assert movie.score == 5.3
 
+#unit test put_info
 def database_test_put_info():
     database.put_info()
     assert "Adventure" in database.genres
@@ -24,7 +26,19 @@ def database_test_put_info():
     assert "Robert Altman" in database.directors
     assert 1980 in database.years
     assert 5.3 in database.scores
-
+    
+#unit test choose_movie
+def database_test_choose_movie():
+    database.choose_movie()
+    #someone else has to add to this because i can't run the final project code
+    
+#unit test display_info
+def database_test_display_info():
+    database.display_info()
+    assert("comedy", "R", 1.75) == "Hot Tub Time Machine"
+    assert("fantasy", "PG", 1.5) == "Shrek"
+    assert("family", "G", 2) == "Cars"
+    assert("horror", "PG-13", 1.5) == "The Boogeyman"
 
 
 
